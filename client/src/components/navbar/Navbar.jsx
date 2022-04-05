cbe7cb3b2a3834aef7e50c9a82f487658c7b3072
Patrick Tonderai Ganhiwa <ptganhiwa8@gmail.com>
2022-04-06 00:08:55 +0200

    done connecting the components using v6rrd

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.scss";
 
 function Navbar() {
@@ -14,9 +15,15 @@ function Navbar() {
         <div className="container">
             <div className="left">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" alt="" />
-                <span>Homepage</span>
-                <span>Series</span>
-                <span>Movies</span>
+                <Link to="/netflix-app-clone-pats/" className="link">
+                    <span>Homepage</span>
+                </Link>
+                <Link to="/netflix-app-clone-pats/series" className="link">
+                    <span>Series</span>
+                </Link>
+                <Link to="/netflix-app-clone-pats/movies" className="link">
+                    <span>Movies</span>
+                </Link>
                 <span>New and Popular</span>
                 <span>My List</span>
             </div>
