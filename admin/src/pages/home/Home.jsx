4adbb809fd18333688ca2dfd53abb319041e28dd
Patrick Tonderai Ganhiwa <ptganhiwa8@gmail.com>
2022-04-10 01:48:16 +0200

    worked on linking home chart with mongodb retrieving stats

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -6,8 +6,48 @@ import Sidebar from '../../components/sidebar/Sidebar';
 import Table from '../../components/table/Table';
 import Widget from '../../components/widget/Widget';
 import "./Home.scss";
+import { useState, useEffect, useMemo } from "react";
+import axios from "axios";
 
 const Home = () => {
+  const MONTHS = useMemo(() => [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+    ],[]
+    );
+    const [userStats, setUserStats] = useState([]);
+  
+    useEffect(() => {
+        const getStats = async () => {
+          try {
+            const res = await axios.get("/users/stats", {
+              Headers: {
+                token: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNGM0MzA0NjZiZjU2YzEwNDdjYmNhZiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0OTU0MzI3OCwiZXhwIjoxNjQ5OTc1Mjc4fQ.sJqE2tpW8YCJLpjGkTqD_3U2amZFMbBPFtXah6MDKvw"
+              },
+            });
+            const statsList = res.data.sort(function (a, b) {
+              return a._id - b._id;
+            });
+            statsList.map(item => setUserStats(prev=>[...prev, {
+              name: MONTHS[item._id - 1], 
+              "Total User": item.total 
+            },]));
+          } catch (err) {
+            console.log(err)
+          }
+        }
+        getStats();
+    }, [MONTHS]);
   return (
     <div className='home'>
         <Sidebar />
@@ -21,7 +61,7 @@ const Home = () => {
           </div>
           <div className="charts">
             <Featured />
-            <Chart title="Last 6 Months (Revenue)" aspect={ 2 / 1} />
+            <Chart title="Last 6 Months (Revenue)" aspect={ 2 / 1} data={userStats} />
           </div>
           <div className="listContainer">
             <div className="listTitle">Latest Transactions</div>
