3ab807979b8ec6d4c53e5f1a1da1849b37e9f11e
Patrick Tonderai Ganhiwa <ptganhiwa8@gmail.com>
2022-04-12 01:32:15 +0200

    worked on the the login page and context api

--- admin/src/pages/login/Login.jsx
+++ admin/src/pages/login/Login.jsx
@@ -1,9 +1,25 @@
+import { useContext, useState } from "react";
+import { login } from "../../context/authContext/APICalls";
+import { AuthContext } from "../../context/authContext/AuthContext";
 import "./Login.scss";
 
 const Login = () => {
+
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+  const { isFetching, dispatch } = useContext(AuthContext);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    login({ email, password }, dispatch);
+  }
   return (
     <div className='login'>
-        Login
+        <form className="loginForm">
+          <input type="text" placeholder="email" className="loginInput" onChange={(e) => setEmail(e.target.value)}/>
+          <input type="password" placeholder="password" className="loginInput" onChange={(e) => setPassword(e.target.value)}/>
+          <button className="loginButton" onClick={handleLogin} disabled={isFetching}>Login</button>
+        </form>
     </div>
   )
 }
